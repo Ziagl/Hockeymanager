4d917bd83d149a53cc58a7840b19ebc3d8b31c88
Ziagl <office@ziegelwanger-edv.at>
2024-05-17 15:03:04 +0200

    rules are now available without login

--- content/header.php
+++ content/header.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
+		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Eishockey to Dream</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
---
+++ rules.php
@@ -0,0 +1,8 @@
+<?php
+include 'header.php';
+
+header('Content-Type: text/html; charset=utf-8');
+include 'rules.html';
+
+include 'footer.php';
+?>
