8486f9cae0bdcb33d348fc1635239ab6ca0db607
Ziagl <office@ziegelwanger-edv.at>
2025-02-20 21:03:33 +0100

    fixed error

--- config/config.php
+++ config/config.php
@@ -14,3 +14,10 @@ $Max_GOALS_OVERTIME = 10;
 $MAX_GOALS_NHL_HOME = 40;
 $MAX_GOALS_NHL_AWAY = 30;
 $Max_GOALS_NHL_OVERTIME = 20;
+
+/*
+// debugging
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+*/
